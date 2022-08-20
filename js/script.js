d25fe7b68ec0a5a3e0d896b6e3196bce86e3fb97
Milleny <ellenmilleny@gmail.com>
2022-08-19 21:43:00 -0300

    Challenge finalizado

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
 window.onload = function(){
+
+    document.getElementById("copiar-texto").addEventListener("click", clipboardCopy);
+    async function clipboardCopy() {
+        let text = document.getElementById("saida").value;
+        await navigator.clipboard.writeText(text);
+        document.getElementById("entrada").value = "";
+        document.getElementById("saida").value = "";
+        alert("Texto copiado!");
+    }
+
     function criptografar(){
-        let frase = textEntrada.value;
 
-        let textoCriptografado = frase
-        .replaceAll("e", "enter")
-        .replaceAll("i", "imes")
-        .replaceAll("a", "ai")
-        .replaceAll("o", "ober")
-        .replaceAll("u","ufat");
+            let frase = textEntrada.value.toLowerCase();
 
-        textSaida.value = textoCriptografado;
+            let textoCriptografado = frase
+            .replaceAll("e", "enter")
+            .replaceAll("i", "imes")
+            .replaceAll("a", "ai")
+            .replaceAll("o", "ober")
+            .replaceAll("u","ufat");
+
+            textSaida.value = textoCriptografado;
+
+            document.getElementById("menino").style.display = "none";
+            document.getElementById("text1").style.display = "none";
+            document.getElementById("text2").style.display = "none";
+            document.getElementById("saida").style.display = "inline";
+            document.getElementById("copiar-texto").style.display = "inline";
+        
     }
 
     function descriptografar(){
-        let frase = textSaida.value;
 
-        let textoDescriptografado = frase
-        .replaceAll("enter", "e")
-        .replaceAll("imes", "i")
-        .replaceAll("ai", "a")
-        .replaceAll("ober", "o")
-        .replaceAll("ufat","u");
+            let frase = textEntrada.value.toLowerCase();
 
-        textSaida.value = textoDescriptografado;
+            let textoDescriptografado = frase
+            .replaceAll("enter", "e")
+            .replaceAll("imes", "i")
+            .replaceAll("ai", "a")
+            .replaceAll("ober", "o")
+            .replaceAll("ufat","u");
+
+            textSaida.value = textoDescriptografado;
+
+            document.getElementById("menino").style.display = "none";
+            document.getElementById("text1").style.display = "none";
+            document.getElementById("text2").style.display = "none";
+            document.getElementById("saida").style.display = "inline";
+            document.getElementById("copiar-texto").style.display = "inline";
+            
     }
 
-    function copiar() {
-        document.getElementById("textSaida").Select();
-        document.execCommand("copy");
-        alert("Text copiado");
+    function verifica() {
+
+        if (document.getElementById("entrada").value === "") {
+            document.getElementById("copiar-texto").style.display = "none";
+            document.getElementById("saida").style.display = "none";
+        } 
     }
 
     let textEntrada = document.getElementById("entrada");
@@ -37,10 +65,9 @@ window.onload = function(){
     let buttonCriptografar = document.getElementById("criptografar");
     let buttonDescriptografar = document.getElementById("descriptografar");
     let buttonCopiar = document.getElementById("copiar-texto");
+    verifica();
 
     buttonCriptografar.onclick = criptografar;
     buttonDescriptografar.onclick = descriptografar;
-    buttonCopiar.onclick = copiar;
-
 
 }
